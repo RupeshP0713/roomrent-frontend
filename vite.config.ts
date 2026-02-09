3d34387fcb99975eda548e074a6ffc7f97005f0e
Patrarupesh9157 <patrarupesh9157@gmail.com>
2026-02-09 18:22:39 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://roomrent-backend.vercel.app',
+        target: 'https://roomrent-backend.vercel.app/api',
         changeOrigin: true
       }
     }
